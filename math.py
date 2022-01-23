44b826c2a9e3d5517ede14f0709782f9b26832b5
Aadarshhsingh <98268027+Aadarshhsingh@users.noreply.github.com>
2022-01-23 19:54:40 +0530

    first commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+# function call
+greet('Paul')
+
+# function definition
+def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    """
+    print("Hello, " + name + ". Good morning!")
+    
+ def absolute_value(num):
+    """This function returns the absolute
+    value of the entered number"""
+
+    if num >= 0:
+        return num
+    else:
+        return -num
